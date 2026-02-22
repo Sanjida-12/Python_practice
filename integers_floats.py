5f7be3e767f825354eb72e0d32534e610699e5da
Sanjida <fsanjida08@gmail.com>
2026-02-23 04:18:30 +0800

    All about integers and floats data type

---
+++ integers_floats.py
@@ -0,0 +1,45 @@
+num = 22 
+print(type(num))
+print(float(num))
+
+#Arithmatic Operators:
+#Additions: 3+2
+#Substraction: 3-2
+#Multipication: 3*2
+#Division: 3/2
+#Floor Division: 3//2 (whole digit)
+#Exponent: 3**2 
+#Modulus: 3%2
+
+#round
+print(round(3.75,1))#this will take upto 1 decimal number which is 3.8
+print(round(3.75467, 3)) 
+
+#abs 
+print(abs(-2))
+print(abs(3.44)) # always will take positive value
+
+#Comparisons
+#Equal: 3==2
+#Not Equal: 3!=2
+#Greater than:  3>2
+#Less than: 3<2
+#Greater or equal: 3>=2
+#less or equal: 3<=2
+
+num_1=3
+num_2=2
+print(num_1 == num_2) # it will show false
+print(num_1 != num_2) #true
+print(num_1 >= num_2) #True
+print(num_1 <= num_2) #False
+
+num_1='100'
+num_2='200'
+print(num_1 + num_2) # sring will add and will get 100200
+
+num_1=int(num_1)
+num_2=int(num_2)
+print(num_1+num_2) # int will specify that 2 variables need to add then we will get 300.
+
+
